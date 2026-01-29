6dd40e8b101e9a0eae3286db0e7830ef7a63f376
ruhtrA-1 <barbosaarthur463@gmail.com>
2026-01-29 10:18:40 -0300

    Comandos git

--- LISTAS_INT/main.c
+++ LISTAS_INT/main.c
@@ -3,8 +3,13 @@
 
 int main()
 {
-    printf("Ola mundo");
-    printf("Recebaaaaa");
+    /*
+        -Comandos Git:
+        1- git add . (salvar tudo)
+        2- git status (ver se salvou o que eu escrevi)
+        3- git commit -m "Mensagem que eu quero xD" (salva no repositório)
+        4- git push (envia para o repositório)
+    */
 
     return 0;
 }
